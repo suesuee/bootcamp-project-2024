24d123d41668cb32a10b3599a9f57dd68162e345
Sue Sue <29suesue@gmail.com>
2024-10-24 10:55:11 -0700

    feat: add a list of Blogs with two elements

---
+++ src/blog.ts
@@ -0,0 +1,27 @@
+type Blog = {
+    title: string;
+    date: string;
+    description: string;
+    image: string;
+    imageAlt: string;
+    slug: string;
+};
+
+const blogs: Blog[] = [
+    {
+        title: "STREAM AESPA WHIPLASH",
+        date: "10/23/2024",
+        description: "GO STREA AESPA WHIPLASH ",
+        image: "aespa-whiplash.jpg",
+        imageAlt: "WHIPLASH <3",
+        slug: "Stream-Whiplash",
+    },
+    {
+        title: "STREAM APT BY ROSE and BRUNO MARS",
+        date: "10/23/2024",
+        description: "STREAM APT BY ROSE and BRUNO MARS",
+        image: "APT.jpg",
+        imageAlt: "APT <3",
+        slug: "Stream-APT",
+    },
+];
