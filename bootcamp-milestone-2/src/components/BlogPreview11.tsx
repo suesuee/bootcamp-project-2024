fff241f6b15f000b790f8de716f70854fe128525
Sue Sue <29suesue@gmail.com>
2024-12-22 02:25:45 -0800

    temp: renmae files to temp names

--- bootcamp-milestone-2/src/components/BlogPreview11.tsx
+++ bootcamp-milestone-2/src/components/BlogPreview11.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./BlogPreview.module.css";
-import type { Blog } from "../static/blogData";
+
+// Define the Blog type based on your database schema
+export type Blog = {
+  title: string;
+  date: string; // Use string to match how the date is stored in your database
+  description: string;
+  image: string;
+  imageAlt: string;
+  slug: string;
+};
 
 export default function BlogPreview({
   title,
@@ -15,14 +24,13 @@ export default function BlogPreview({
   return (
     <div className={styles.blogPreview}>
       <h3>
-        <Link href={`/blog/${slug}`}>{title}</Link>{" "}
-        {/* ! Link to dynamic blog page */}
+        <Link href={`/blog/${slug}`}>{title}</Link>
       </h3>
       <div className={styles.imageContainer}>
         <Image src={image} alt={imageAlt} width={500} height={300} />
       </div>
       <p>
-        <strong>Date:</strong> {date}
+        <strong>Date:</strong> {new Date(date).toLocaleDateString()}
       </p>
       <p>{description}</p>
     </div>
