44dd2debc5a6252794479fe1b4c57c8a9cda1d63
Sue Sue <29suesue@gmail.com>
2024-12-22 01:00:56 -0800

    fix: remove sentences

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -14,12 +14,8 @@ export default function HomePage() {
           className={styles.profileImage}
         />
         <p className={styles.description}>
-          Hi, I'm <strong>Sue</strong>, a Computer Science student at California Polytechnic State University.
+          Hello, I'm <strong>Sue</strong>, a Computer Science student at California Polytechnic State University.
           I am from Myanmar, and I am a third-year transfer student from De Anza College in Cupertino.
-          I'm passionate about technology, particularly in AI, software development, and hands-on projects.
-          I'm always looking for new challenges to learn and grow, whether it's working on coding projects or
-          exploring digital tools like 3D printing. This site showcases my projects, experiences, and what I'm working
-          on as I continue my journey in tech.
         </p>
       </div>
     </div>
